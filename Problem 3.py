97edd4b1416f1fc56044354678fe8a46efcb4f75
Your Name <you@example.com>
2020-04-05 01:19:34 +0300

    Create Problem 3.py

--- Problem 3.py
+++ Problem 3.py
@@ -5,29 +5,23 @@ Created on Sat Apr  4 23:32:25 2020
 @author: kjiwj
 """
 
-def isPrime(n):
-    if (n <= 1):
+def isPrime(n): 
+    if n < 2: 
         return False
-    if (n <= 3):
-        return True
-    if (n % 2 == 0 or n % 3 == 0):
-        return False
-    i = 6
-    while(i * i <= n):
-        if (n % (i - 1) == 0 or n % (i + 1) == 0):
+    for i in range(2, int(n**0.5) + 1): 
+        if n % i == 0: 
             return False
-        i = i + 6
     return True
 
 number = 600851475143
 
-n = 1 #ignore 2
+n = 1
 
 while n < number:
     if number % n == 0:
         if isPrime(n):
             number //= n
             continue
-    n += 2
+    n += 1
 
 print(n)

ccfaed7e6813996acb73ff47b0dbdcce32f60ef2
Your Name <you@example.com>
2020-04-04 23:22:14 +0300

    Create Problem 2.py

---
+++ Problem 2.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr  4 22:50:32 2020
+
+@author: kjiwj
+"""
+
+def fibonacci(n) :
+    if n <= 1 :
+        return n
+    else:
+        return(fibonacci(n - 1) + fibonacci(n - 2))
+
+sum = 0
+n = 1
+
+while fibonacci(n) <= 4000000 :
+    if fibonacci(n) % 2 == 0 :
+        sum += fibonacci(n)
+    n += 1
+    
+print(sum)
